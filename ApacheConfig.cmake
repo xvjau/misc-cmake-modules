c8f9872702831ab984b07d804661e0d643e4f513
Gianni Rossi <gianni.rossi@transtelco.com.br>
2012-09-04 15:37:34 -0300

    Fixed include guard and added apache inlcude dir.

--- ApacheConfig.cmake
+++ ApacheConfig.cmake
@@ -5,8 +5,7 @@
 #  ApacheAPR_INCLUDE_DIR - the ApacheAPR include directory
 #  ApacheAPR_LINK_FLAGS - Link flags to use ApacheAPR
 
-
-if (NOT ApacheAPR_INCLUDE_DIR AND ApacheAPR_LINK_FLAGS )
+if (NOT (ApacheAPR_INCLUDE_DIR OR ApacheAPR_LINK_FLAGS ))
 	set(ApacheAPR_FOUND FALSE)
 
 	execute_process(COMMAND apr-1-config --includedir RESULT_VARIABLE __apache_apr_ret OUTPUT_VARIABLE ApacheAPR_INCLUDE_DIR OUTPUT_STRIP_TRAILING_WHITESPACE)
@@ -16,7 +15,9 @@ if (NOT ApacheAPR_INCLUDE_DIR AND ApacheAPR_LINK_FLAGS )
 			set(ApacheAPR_FOUND TRUE)
 		endif()
 	endif()
-	
+
+	find_path(ApacheINCLUDE_DIR httpd.h HINTS /usr/include/apache2)
+
 	if(${ApacheAPR_FOUND})
 		mark_as_advanced(ApacheAPR_INCLUDE_DIR ApacheAPR_LINK_FLAGS)
 	endif()
